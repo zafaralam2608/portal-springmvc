a2734a00cc44e01b8d002d89e0e92dd92dd1c835
Zafar <zafaralam2608@gmail.com>
2020-02-21 22:03:31 +0530

    Initial commit

---
+++ portal-webapp/src/main/java/com/project/portal/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.project.portal.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.project.portal")
+public class WebConfig implements WebMvcConfigurer {
+
+}
